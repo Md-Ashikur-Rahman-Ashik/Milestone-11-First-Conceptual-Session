d2f97632f1b16a951af75889c0b5cc2a4e429779
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-04 16:41:15 +0600

    Added login page
    Also, adding registration page

--- solosphere-client/src/components/Navbar.jsx
+++ solosphere-client/src/components/Navbar.jsx
@@ -4,15 +4,15 @@ import { AuthContext } from "../provider/AuthProvider";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
 
   return (
     <div className="navbar bg-base-100 shadow-sm container px-4 mx-auto">
       <div className="flex-1">
-        <div className="flex gap-2 items-center">
+        <Link to="/" className="flex gap-2 items-center">
           <img className="w-auto h-7" src={logo} alt="" />
           <span className="font-bold">SoloSphere</span>
-        </div>
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
@@ -34,11 +34,11 @@ const Navbar = () => {
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-              <div className="w-10 rounded-full" title="">
+              <div title={user?.displayName} className="w-10 rounded-full">
                 <img
                   referrerPolicy="no-referrer"
                   alt="User Profile Photo"
-                  src=""
+                  src={user.photoURL}
                 />
               </div>
             </div>
@@ -58,7 +58,7 @@ const Navbar = () => {
               <li>
                 <div>Bid Requests</div>
               </li>
-              <li className="mt-2">
+              <li className="mt-2" onClick={logOut}>
                 <button className="bg-gray-200 block text-center">
                   Logout
                 </button>
